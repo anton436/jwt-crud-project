b42f308afc9428b5579236fdaf7393387ae0dc84
anton <oneshot436@gmail.com>
2023-02-07 18:51:45 +0600

    .

--- src/components/product/AddProduct.jsx
+++ src/components/product/AddProduct.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 
 const AddProduct = () => {
-  return <div>AddProduct</div>;
+  return (
+    <div className='d-flex flex-column w-50 m-auto'>
+      <h1>Add product</h1>
+
+      <select name='' id=''>
+        <option value=''>choose category</option>
+      </select>
+      <input type='text' placeholder='title' />
+      <input type='text' placeholder='description' />
+      <input type='text' placeholder='price' />
+      <input type='file' />
+      <button>Add product</button>
+    </div>
+  );
 };
 
 export default AddProduct;
